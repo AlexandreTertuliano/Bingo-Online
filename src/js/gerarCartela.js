2de694c8b9ff8bd2c0cdc8e1db30898265016205
Alexandre Tertuliano <38267460+AlexandreTertuliano@users.noreply.github.com>
2021-02-19 17:34:15 -0300

    App completo

---
+++ src/js/gerarCartela.js
@@ -0,0 +1,50 @@
+function gerarCartela(){
+    // document.getElementById("menu-cartela").style = "display:none";
+    // document.getElementById("instrucoes").style = "display:block";
+    var tableSpace = document.getElementById("cartela");
+    tableSpace.innerHTML = "";
+    var table = document.createElement('table');
+    var tr;
+    var td;
+    var num; 
+    var numArr = [];
+    tr = document.createElement('tr');
+    tr.innerHTML= '<th> B </th><th> I </th><th> N </th><th> G </th><th> O</th>';
+    table.appendChild(tr);
+    for ( let i = 0; i < 5; i++){
+        tr = document.createElement('tr');
+        for ( let j = 0; j < 5; j++){
+            num = gerarNumeroAleatorio(75);
+            posicao = verificaPosicao(numArr.length)
+            min = ((posicao-1)*15)+1;
+            max = posicao*15;
+            if(numArr.indexOf(num) > -1 || num == 0 || num < min || num > max ){
+                j--;
+            }      
+            else{
+                numArr.push(num);
+                td = document.createElement('td');
+                if(numArr.length != 13){
+                    td.addEventListener("click",function(){
+                        this.classList.add("marked");
+                    });
+                }
+                if(numArr.length == 13){
+                    td.classList.add("central");
+                    td.classList.add("coringa");
+                    // td.innerHTML ;
+                }
+                else{
+                    
+                    td.innerHTML += "<h1> "+ num + "</h1>";
+                }
+                tr.appendChild(td);
+            }
+            
+        }
+        table.appendChild(tr);
+    }  
+    tableSpace.appendChild(table);
+    
+    document.getElementById("gerar").setAttribute("disabled","disabled");
+}
